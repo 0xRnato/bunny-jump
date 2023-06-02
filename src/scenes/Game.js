670543b9b879cf1c17d7c78f7f57bf8eddeec667
Renato Neto <rnato.netoo@gmail.com>
2023-06-01 22:25:58 -0300

    feat: reuse bottom platforms after camera scroll

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,6 +3,8 @@ import Phaser from '../lib/phaser.js'
 export default class Game extends Phaser.Scene {
   /** @type {Phaser.Physics.Arcade.Sprite} */
   player
+  /** @type {Phaser.Physics.Arcade.StaticGroup} */
+  platforms
 
   constructor() {
     super('game')
@@ -15,14 +17,14 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(240, 320, 'background')
+    this.add.image(240, 320, 'background').setScrollFactor(1, 0)
 
-    const platforms = this.physics.add.staticGroup()
+    this.platforms = this.physics.add.staticGroup()
     for (let i = 0; i < 5; ++i) {
       const x = Phaser.Math.Between(80, 400)
       const y = 150 * i;
       /** @type {Phaser.Physics.Arcade.Sprite} */
-      const platform = platforms.create(x, y, 'platform')
+      const platform = this.platforms.create(x, y, 'platform')
       platform.scale = 0.5
       /** @type {Phaser.Physics.Arcade.StaticBody} */
       const body = platform.body
@@ -30,7 +32,7 @@ export default class Game extends Phaser.Scene {
     }
 
     this.player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5)
-    this.physics.add.collider(platforms, this.player)
+    this.physics.add.collider(this.platforms, this.player)
     this.player.body.checkCollision.up = false
     this.player.body.checkCollision.left = false
     this.player.body.checkCollision.right = false
@@ -39,6 +41,16 @@ export default class Game extends Phaser.Scene {
   }
 
   update() {
+    this.platforms.children.iterate(child => {
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = child
+      const scrollY = this.cameras.main.scrollY
+      if (platform.y >= scrollY + 700) {
+        platform.y = scrollY - Phaser.Math.Between(50, 100)
+        platform.body.updateFromGameObject()
+      }
+    })
+
     const touchingDown = this.player.body.touching.down
     if (touchingDown) {
       this.player.setVelocityY(-300)
